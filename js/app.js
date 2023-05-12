69880ec2cd39dc50fc5f4c18867a38ea6ee13ea6
Vishal Maurya <vishalmaurya12nov@gmil.com>
2023-05-12 11:17:46 +0530

    update rating hover

--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
 $(".star_rating").each(function (index, element) {
   $(element).click(() => {
     $(element).toggleClass('color')
-    // $(selector).toggleClass(className);
-    console.log($(element).text());
+    // console.log($(element).text());
     $('.get_rating').text( `${$(element).text()}`);
   });
 });
 $('.rating_card__submit_btn').click(()=> { 
- 
   $(".thankyou_card").addClass('show_thanks');
-  $(".rating_card").addClass('hide_rating_card');
-  
+  $(".rating_card").addClass('hide_rating_card'); 
 });
